49647602f76cfe8dd66e904246ce0d233db9eaf7
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2024-12-17 11:09:37 -0700

    finalizing init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inventory from "./pages/Inventory/Inventory.jsx";
+import InventoryAdd from "./pages/InventoryAdd/InventoryAdd.jsx";
+import InventoryDetails from "./pages/InventoryDetails/InventoryDetails.jsx";
+import InventoryEdit from "./pages/InventoryEdit/InventoryEdit.jsx";
+import Warehouse from "./pages/Warehouse/Warehouse.jsx";
+import WarehouseAdd from "./pages/WarehouseAdd/WarehouseAdd.jsx";
+import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails.jsx";
+import WarehouseEdit from "./pages/WarehouseEdit/WarehouseEdit.jsx";
 
 function App() {
 
   return (
-    <>
-      <h1>CODE CRUSADERS</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/warehouse' element={<Warehouse/>}/>
+        <Route path='/warehouseadd' element={<WarehouseAdd/>}/>
+        <Route path='/warehousedetails' element={<WarehouseDetails/>}/>
+        <Route path='/warehouseedit' element={<WarehouseEdit/>}/>
+        <Route path='/inventory' element={<Inventory/>}/>
+        <Route path='/inventoryadd' element={<InventoryAdd/>}/>
+        <Route path='/inventorydetails' element={<InventoryDetails/>}/>
+        <Route path='/inventoryedit' element={<InventoryEdit/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
