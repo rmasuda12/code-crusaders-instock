c1e74a55ad3329b256376a8df48db2b8567f4d1c
mishalfatimag <107395170+mishalfatimag@users.noreply.github.com>
2024-12-17 15:51:58 -0500

    warehousepage in progress

---
+++ src/pages/WarehouseAdd/WarehouseAdd.jsx
@@ -0,0 +1,14 @@
+import "./Warehouse.scss";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const WarehouseValues = {
+  warehouse_name: "",
+  address: "",
+  contactname: "",
+  contactinformation: "",
+  actions: "",
+};
+
+export default "WarehouseAdd";
