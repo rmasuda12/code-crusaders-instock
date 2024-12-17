9f711d512574aa9994ce1074a81daf0dd3eac901
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2024-12-17 09:51:24 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>CODE CRUSADERS</h1>
+    </>
+  )
+}
+
+export default App
