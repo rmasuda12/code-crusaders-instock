06b2b98a8736abb3f1fb5ad97bce8a3613b8fcaa
Rintaro Masuda <66854464+rmasuda12@users.noreply.github.com>
2024-12-18 11:08:36 -0700

    modify text of delete modal to be responsive

--- src/components/Delete/Delete.jsx
+++ src/components/Delete/Delete.jsx
@@ -9,7 +9,7 @@ function Delete() {
                     <img src="src/assets/icons/close-24px.svg"/>
                 </div>
                 <h1 className="modal__title">Delete LOCATION warehouse?</h1>
-                <p className="modal__text p1">Please confirm that you'd like to delete the LOCATION from the list of <br></br>warehouses. You won't be able to undo this action.</p>
+                <p className="modal__text p1">Please confirm that you'd like to delete the LOCATION from the list of warehouses. You won't be able to undo this action.</p>
                 <div className="modal__container">
                     <button className="modal__button modal__button--cancel">Cancel</button>
                     <button className="modal__button modal__button--delete">Delete</button>                    
