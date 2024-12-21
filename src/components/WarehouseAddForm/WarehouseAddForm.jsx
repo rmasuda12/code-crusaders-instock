623c82eb70a5cfccee26909c3fa1dc7aeb74a53f
Pavan Sandhu <pavanbsandhu@gmail.com>
2024-12-20 19:59:20 -0700

    warehouse add progress update

--- src/components/WarehouseAddForm/WarehouseAddForm.jsx
+++ src/components/WarehouseAddForm/WarehouseAddForm.jsx
@@ -17,22 +17,53 @@ function WarehouseAddForm() {
     phoneNumber: "",
     email: "",
   });
-  const [warehouseData, setWarehouseData] = useState(formData);
 
-  const handleInputChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setFormData((prevData) => {
-      const updatedData = { ...prevData };
-      updatedData[name] = value;
-      return updatedData;
-    });
+  const handleChangeForm = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit");
+
+    function validateForm(formData) {
+      let isFormValid = true;
+
+      for (let key in formData) {
+        const fieldValue = formData[key];
+        if (fieldValue === "") {
+          isFormValid = false;
+          break;
+        }
+      }
+
+      if (!isFormValid) {
+        alert("Please do not leave any fields empty");
+      }
+      return isFormValid;
+    }
+    const isFormValid = validateForm(formData);
+    if (!isFormValid) {
+      return;
+    }
+
+    // const { updated_at, created_at, ...dataToSend } = formData; change date format being submitted
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/warehouses/${params.id}`,
+        dataToSend
+      );
+      console.log("Warehouse updated successfully:", response);
+    } catch (error) {
+      console.log(error);
+      alert("Error updating Warehouse");
+    }
   };
+
   const handleCancelClick = (e) => {
     e.preventDefault();
     navigate("/warehouse");
@@ -40,113 +71,106 @@ function WarehouseAddForm() {
 
   return (
     <>
-      <div className="wh-edit">
-        <form className="wh-edit__form" onSubmit={handleSubmit}>
-          <div className="wh-edit__form-container">
-            <h2 className="wh-edit__details-header">Warehouse Details</h2>
-            <label className="wh-edit__form-label">
-              <h3>Warehouse Name</h3>
-            </label>
-            <input
-              type="text"
-              name="warehouseName"
-              onChange={handleInputChange}
-              value={formData.warehouseName}
-              placeholder={warehouseData.warehouse_name}
-              className="wh-edit__formfield"
-            />
-            <label className="wh-edit__form-label">
-              <h3>Street Address</h3>
-            </label>
-            <input
-              type="text"
-              name="streetAddress"
-              onChange={handleInputChange}
-              value={formData.streetAddress}
-              placeholder={warehouseData.address}
-              className="wh-edit__formfield"
-            />
-            <label className="wh-edit__form-label">
-              <h3>City</h3>
-            </label>
-            <input
-              type="text"
-              name="city"
-              onChange={handleInputChange}
-              value={formData.city}
-              placeholder={warehouseData.city}
-              className="wh-edit__formfield"
-            />
-            <label className="wh-edit__form-label">
-              <h3>Country</h3>
-            </label>
-            <input
-              type="text"
-              name="country"
-              onChange={handleInputChange}
-              value={formData.country}
-              placeholder={warehouseData.country}
-              className="wh-edit__formfield"
-            />
+      <div className="wh-new">
+        <form className="wh-new__form" onSubmit={handleSubmit}>
+          <div className="test">
+            <div className="wh-new__form-container">
+              <h2 className="wh-new__details-header">Warehouse Details</h2>
+              <label className="wh-new__form-label">
+                <h3>Warehouse Name</h3>
+              </label>
+              <input
+                type="text"
+                name="warehouse_name"
+                onChange={handleChangeForm}
+                value={formData.warehouse_name}
+                className="wh-new__formfield"
+              />
+              <label className="wh-new__form-label">
+                <h3>Street Address</h3>
+              </label>
+              <input
+                type="text"
+                name="address"
+                onChange={handleChangeForm}
+                value={formData.address}
+                className="wh-new__formfield"
+              />
+              <label className="wh-new__form-label">
+                <h3>City</h3>
+              </label>
+              <input
+                type="text"
+                name="city"
+                onChange={handleChangeForm}
+                value={formData.city}
+                className="wh-new__formfield"
+              />
+              <label className="wh-new__form-label">
+                <h3>Country</h3>
+              </label>
+              <input
+                type="text"
+                name="country"
+                onChange={handleChangeForm}
+                value={formData.country}
+                className="wh-new__formfield"
+              />
+            </div>
+            <div className="wh-new__form-container">
+              <h2 className="wh-new__details-header">Contact Details</h2>
+              <label className="wh-new__form-label">
+                <h3>Contact Name</h3>
+              </label>
+              <input
+                type="text"
+                name="contact_name"
+                onChange={handleChangeForm}
+                value={formData.contact_name}
+                className="wh-new__formfield"
+              />
+              <label className="wh-new__form-label">
+                <h3>Position</h3>
+              </label>
+              <input
+                type="text"
+                name="contact_position"
+                onChange={handleChangeForm}
+                value={formData.contact_position}
+                className="wh-new__formfield"
+              />
+              <label className="wh-new__form-label">
+                <h3>Phone Number</h3>
+              </label>
+              <input
+                type="text"
+                name="contact_phone"
+                onChange={handleChangeForm}
+                value={formData.contact_phone}
+                className="wh-new__formfield"
+              />
+              <label className="wh-new__form-label">
+                <h3>Email</h3>
+              </label>
+              <input
+                type="text"
+                name="contact_email"
+                onChange={handleChangeForm}
+                value={formData.contact_email}
+                className="wh-new__formfield"
+              />
+            </div>
           </div>
-          <div className="wh-edit__form-container">
-            <h2 className="wh-edit__details-header">Contact Details</h2>
-            <label className="wh-edit__form-label">
-              <h3>Contact Name</h3>
-            </label>
-            <input
-              type="text"
-              name="contactName"
-              onChange={handleInputChange}
-              value={formData.contactName}
-              placeholder={warehouseData.contact_name}
-              className="wh-edit__formfield"
-            />
-            <label className="wh-edit__form-label">
-              <h3>Position</h3>
-            </label>
-            <input
-              type="text"
-              name="position"
-              onChange={handleInputChange}
-              value={formData.position}
-              placeholder={warehouseData.contact_position}
-              className="wh-edit__formfield"
-            />
-            <label className="wh-edit__form-label">
-              <h3>Phone Number</h3>
-            </label>
-            <input
-              type="text"
-              name="phoneNumber"
-              onChange={handleInputChange}
-              value={formData.phoneNumber}
-              placeholder={warehouseData.contact_phone}
-              className="wh-edit__formfield"
-            />
-            <label className="wh-edit__form-label">
-              <h3>Email</h3>
-            </label>
-            <input
-              type="text"
-              name="email"
-              onChange={handleInputChange}
-              value={formData.email}
-              placeholder={warehouseData.contact_email}
-              className="wh-edit__formfield"
-            />
-          </div>
-
-          <section className="wh-edit__cta">
+          <section className="wh-new__cta">
             <button
-              className="wh-edit__button-reset"
+              className="wh-new__button-reset"
               type="reset"
               onClick={handleCancelClick}
             >
-              <h3 className="wh-edit__cancel-text">Cancel</h3>
+              <h3 className="wh-new__cancel-text">Cancel</h3>
             </button>
-            <button className="wh-edit__button-save" type="submit">
-              <h3 className="wh-edit__button-text">Save</h3>
+            <button className="wh-new__button-save" type="submit">
+              <h3 className="wh-new__button-text">Save</h3>
             </button>
           </section>
         </form>
