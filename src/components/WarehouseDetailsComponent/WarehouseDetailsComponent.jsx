b3e718a28ec08bb948bcdcc203c16c2e40d6d7e0
vanessa-tm <vanessatmiller@gmail.com>
2024-12-21 10:03:46 -0700

    push daily update

--- src/components/WarehouseDetailsComponent/WarehouseDetailsComponent.jsx
+++ src/components/WarehouseDetailsComponent/WarehouseDetailsComponent.jsx
@@ -23,20 +23,20 @@ function WarehouseDetailsComponent() {
         <>
             <section className='details'>
                 <section className='details__info'>
-                    <Link to={'/warehouse'} className='details__arrow'>
+                    <Link to={'/warehouses'} className='details__arrow'>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z" fill="#2E66E6" />
                         </svg>
                     </Link>
 
                     <h1 className='details__city'>{warehouseDetails.warehouse_name}</h1>
-                    <Link to={'/warehouseedit'} className='details__edit'>
+                    <Link to={`/warehouseedit/${id}`} className='details__edit'>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#FFFFFF" />
                         </svg>
                     </Link>
 
-                    <Link to={'/warehouseedit'} className='details__edit--hidden'>
+                    <Link to={`/warehouseedit/${id}`} className='details__edit--hidden'>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#FFFFFF" />
                         </svg>Edit
--- src/components/WarehouseInventoryItems/WarehouseInventoryItems.jsx
+++ src/components/WarehouseInventoryItems/WarehouseInventoryItems.jsx
@@ -50,16 +50,18 @@ function WarehouseInventoryItems ({warehouseId}) {
                 <h4>ACTIONS</h4>
             </section>
         
-            <div>
-        <section className='inventory-details'>        
+            
+        <section className='inventory-details'>   
             {inventoryDetails.map((item) => (
             <section key={item.id} className="inventory-details__container">
                     <h4 className='inventory-details__items'>INVENTORY ITEM:</h4>
-                    <h3 className='inventory-details__click-item column'>{item.item_name} 
-                        <svg className='inventory-details__chevron' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.99997 6L8.58997 7.41L13.17 12L8.58997 16.59L9.99997 18L16 12L9.99997 6Z" fill="#2E66E6"/>
-                        </svg>
-                    </h3>
+                    {/* <Link to={`/warehouses/${warehouseId}/inventory/${item.id}`}> */}
+                        <h3 className='inventory-details__click-item column'>{item.item_name} 
+                            <svg className='inventory-details__chevron' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.99997 6L8.58997 7.41L13.17 12L8.58997 16.59L9.99997 18L16 12L9.99997 6Z" fill="#2E66E6"/>
+                            </svg>
+                        </h3>
+                    {/* </Link> */}
                     <h4 className='inventory-details__items'>CATEGORY:</h4>
                     <p className='inventory-details__click-item-value column'>{item.category}</p>
 
@@ -73,22 +75,32 @@ function WarehouseInventoryItems ({warehouseId}) {
                     <p className='inventory-details__quantity-value column'>{item.quantity}</p>
            
             <section className='inventory-details__icons'>
-                <p className='inventory-details__delete'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="#C94515"/>
-                    </svg>
-                </p>
-                <p className='inventory-details__edit'>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#2E66E6"/>
-                    </svg>
-                </p>
+                <Link>
+                    <p className='inventory-details__delete'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="#C94515"/>
+                        </svg>
+                    </p>
+                </Link>
+                <Link>
+                    <p className='inventory-details__edit'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#2E66E6"/>
+                        </svg>
+                    </p>
+                </Link>
+              
             </section>
-            </section>
-            ))}
-           </section> 
-           </div> 
+            
         
+
+
+        
+        </section>
+            ))}
+           
+           
+        </section>
 
 
         </>
